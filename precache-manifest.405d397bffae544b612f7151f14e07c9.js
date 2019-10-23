33dd01dc35a5051d83c59d75559af359ba89f88d
JonAmparo <jonathanmamparo@gmail.com>
2019-10-22 20:59:51 -0400

    Updates

---
+++ precache-manifest.405d397bffae544b612f7151f14e07c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d2c17f3c34842bdd2f1c6d5abd6aad6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d991f3162fb56dde4ff",
+    "url": "/static/css/main.33283531.chunk.css"
+  },
+  {
+    "revision": "21f5e89bf74af77be6f3",
+    "url": "/static/js/2.1b942129.chunk.js"
+  },
+  {
+    "revision": "2d991f3162fb56dde4ff",
+    "url": "/static/js/main.b94341a8.chunk.js"
+  },
+  {
+    "revision": "c83f1368d311e59c57b4",
+    "url": "/static/js/runtime-main.3ee827b8.js"
+  }
+]);
